43370d9b06816d120881571688147ce9ee84bc63
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-03-04 18:08:54 -0800

    Script and actions workflow for linting

---
+++ scripts/black-changed-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BLACK_OPTIONS="-t py38 -t py39 -t py310 -t py311 --line-length=120 --skip-string-normalization"
+
+PY_FILES="$(git diff --name-only --diff-filter=ACMRTUXB master | grep -E '.py$')"
+
+if [ -z "$PY_FILES" ]; then
+  exit 0
+fi
+
+black $BLACK_OPTIONS $PY_FILES "$@"
