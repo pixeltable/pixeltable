0b982a64f6b478ff9bb1167c399e4ed7b1c95edd
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-03-05 09:29:26 -0800

    Use idiomatic SQLAlchemy expressions in converter

--- pixeltable/metadata/converters/convert_10.py
+++ pixeltable/metadata/converters/convert_10.py
@@ -1,19 +1,17 @@
 import sqlalchemy as sql
 
+from pixeltable.metadata.schema import Table, TableSchemaVersion
 from pixeltable.metadata import register_converter
-from pixeltable.metadata.schema import Table
+
 
 def convert_10(engine: sql.engine.Engine) -> None:
-    # TODO As a first cut, this is implemented using raw SQL statements.
-    # It would perhaps be better to implement it using idiomatic SQLAlchemy.
-    default_table_attrs = '{"attrs": {"comment": "", "num_retained_versions": 10}}'
-    with engine.connect() as conn:
+    default_table_attrs = {"attrs": {"comment": "", "num_retained_versions": 10}}
+    with engine.begin() as conn:
         # Because `parameters` wasn't actually used for anything,
         # we can simply delete it without any data loss.
-        conn.execute(sql.text("UPDATE tables SET md = md - 'parameters';"))
+        conn.execute(sql.update(Table).values(md=Table.md - 'parameters'))
         # Add `attrs` to all elements of schematableversions.
-        conn.execute(sql.text(f"UPDATE tableschemaversions SET md = md || '{default_table_attrs}';"))
-        conn.commit()
+        conn.execute(sql.update(TableSchemaVersion).values(md=Table.md.concat(default_table_attrs)))
     return
 
 
