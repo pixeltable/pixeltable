09e012e0ae4a8ca0f45cb470230edf8dfc8fb4aa
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-03-08 01:22:30 -0800

    Try using Ruff as an alternative to Flake8/Black

--- scripts/format-changed-files.sh
+++ scripts/format-changed-files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-BLACK_OPTIONS="-t py38 -t py39 -t py310 -t py311 --line-length=120 --skip-string-normalization"
-
 PY_FILES="$(git diff --name-only --diff-filter=ACMRTUXB origin/master | grep -E '.py$')"
 
 if [ -z "$PY_FILES" ]; then
   exit 0
 fi
 
-black $BLACK_OPTIONS $PY_FILES "$@"
+ruff format "$PY_FILES"
--- scripts/lint-changed-files.sh
+++ scripts/lint-changed-files.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-PYLINT_ARGS="--max-line-length 120"
+# This script will lint the .py files that differ from master. It is intended to be
+# run against a PR in CI, linting just the files affected by the PR. It can also be
+# run in a dev environment in order to replicate the linting behavior in CI.
 
-FLAKE8_ARGS="--max-line-length 120"
-
-# Disable various Pylint messages that seem pointless or annoying.
-# If Pylint is bugging you about something that seems like it should be excluded,
-# propose it as a new exclusion by adding it to this list as part of the PR.
-# C0114: Missing module docstring (missing-module-docstring)
-# C0116: Missing function or method docstring (missing-function-docstring)
-# E1121: Too many positional arguments for method call (too-many-function-args)
-# R0401: Cyclic import
-# R0801: Similar lines in 2 files
-# R0902: Too many instance attributes
-# R0913: Too many arguments
-# R0914: Too many local variables
-# W0511: TODO
-DISABLED_PYLINT_MESSAGES="C0114,C0116,E1121,R0401,R0801,R0902,R0913,R0914,W0511"
-
-# This will lint ONLY the files that differ from master. If run against a PR as
-# part of CI, it will lint just the files affected by the PR.
 PY_FILES="$(git diff --name-only --diff-filter=ACMRTUXB origin/master | grep -E '.py$')"
-
-SCRIPT_DIR="$(dirname "$0")"
 
 echo -e "\n============= Running pixeltable linting script."
 
@@ -32,16 +14,17 @@ if [ -z "$PY_FILES" ]; then
 fi
 
 echo -e '\n============= The following files differ from `master` and will be linted:\n'
+
 echo "$PY_FILES"
 
-echo -e "\n============= Running pylint on changed files.\n"
+echo -e '\n============= Running `pylint` on changed files.\n'
 
-pylint $PYLINT_ARGS --disable "$DISABLED_PYLINT_MESSAGES" $PY_FILES
+pylint "$PY_FILES"
 
-echo -e "\n============= Running flake8 on changed files.\n"
+echo -e '\n============= Running `ruff check` on changed files.\n'
 
-flake8 $FLAKE8_ARGS $PY_FILES
+ruff check "$PY_FILES"
 
-echo -e "\n============= Running black --check.\n"
+echo -e '\n============= Running `ruff format --check` on changed files.\n'
 
-"$SCRIPT_DIR"/black-changed-files.sh --check
+ruff format --check "$PY_FILES"
