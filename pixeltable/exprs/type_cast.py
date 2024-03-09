b6ee40dde0c2cc4551118e7e43edc62fe49ceb2c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-03-09 12:48:03 -0800

    Clean up TypeCast

--- pixeltable/exprs/type_cast.py
+++ pixeltable/exprs/type_cast.py
@@ -10,26 +10,13 @@ from .row_builder import RowBuilder
 
 class TypeCast(Expr):
     """
-    An `Expr` that represents an explicit type conversion from an underlying `Expr` to
+    An `Expr` that represents a type conversion from an underlying `Expr` to
     a specified `ColumnType`.
-
-    Currently, the only type conversions supported are:
-    - `StringType` to `JsonType`
-    - Any `T` to `StringType`
-    - Any `T` to a supertype of `T`
-    This may be extended in the future.
     """
-    # TODO: Support other type conversions as needed.
     def __init__(self, underlying: Expr, new_type: ts.ColumnType):
         super().__init__(new_type)
-        if self.col_type.is_supertype_of(underlying.col_type) \
-                or self.col_type.is_string_type() \
-                or self.col_type.is_json_type() and underlying.col_type.is_string_type():
-            # It's a valid type conversion
-            self.components: List[Expr] = [underlying]
-            self.id: Optional[int] = self._create_id()
-        else:
-            raise RuntimeError(f'Expression of type `{self._underlying.col_type}` cannot be cast to `{self.col_type}`')
+        self.components: List[Expr] = [underlying]
+        self.id: Optional[int] = self._create_id()
 
     @property
     def _underlying(self):
@@ -48,27 +35,10 @@ class TypeCast(Expr):
         questions about consistency of doing type conversions in both Python and Postgres.
         """
         return None
-        # underlying_sql_expr = self._underlying.sql_expr()
-        # if underlying_sql_expr is None:
-        #     return None
-        # else:
-        #     return underlying_sql_expr.cast(self.col_type.to_sa_type)
 
     def eval(self, data_row: DataRow, row_builder: RowBuilder) -> None:
         original_val = data_row[self._underlying.slot_idx]
-        if self.col_type.is_supertype_of(self._underlying.col_type):
-            # No conversion necessary
-            cast_val = original_val
-        elif self.col_type.is_string_type() and self._underlying.col_type.is_json_type():
-            # For converting JSON -> string, we need to use json.dumps, not call str(dict)
-            cast_val = json.dumps(original_val)
-        elif self.col_type.is_string_type():
-            cast_val = str(original_val)
-        elif self.col_type.is_json_type() and self._underlying.col_type.is_string_type():
-            cast_val = json.loads(original_val)
-        else:
-            assert False    # This should have been caught in __init__
-        data_row[self.slot_idx] = cast_val
+        data_row[self.slot_idx] = self.col_type.create_literal(original_val)
 
     def _as_dict(self) -> Dict:
         return {'new_type': self.col_type.as_dict(), **super()._as_dict()}
