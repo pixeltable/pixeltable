af9e90f9f83926e493f89b6afed7ecf0150b226b
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-04-18 22:22:13 -0700

    First cut at a yolox udf

--- pixeltable/functions/yolox.py
+++ pixeltable/functions/yolox.py
@@ -9,11 +9,13 @@ from yolox.exp import get_exp, Exp
 from yolox.models import YOLOX
 from yolox.utils import postprocess
 
+import pixeltable as pxt
 from pixeltable import env
 
 
+@pxt.udf
 def yolox(image: PIL.Image.Image, *, model_id: str, device: str = 'cpu') -> dict:
-    model, exp = __lookup_model(model_id)
+    model, exp = __lookup_model(model_id, device)
 
     image_transform, _ = __val_transform(np.array(image), None, exp.test_size)
     image_tensor = torch.from_numpy(image_transform).unsqueeze(0).float()
@@ -24,15 +26,20 @@ def yolox(image: PIL.Image.Image, *, model_id: str, device: str = 'cpu') -> dict
         outputs = postprocess(
             outputs, 1, exp.test_conf, exp.nmsthre, class_agnostic=True
         )
-    return outputs
+
+    return {
+        'boxes': [output[:4].tolist() for output in outputs[0]],
+        'scores': [output[4].item() * output[5].item() for output in outputs[0]]
+    }
 
 
-def __lookup_model(model_id: str) -> (YOLOX, Exp):
-    if model_id in __model_cache:
-        return __model_cache[model_id]
+def __lookup_model(model_id: str, device: str) -> (YOLOX, Exp):
+    key = (model_id, device)
+    if key in __model_cache:
+        return __model_cache[key]
 
     weights_url = f'https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/{model_id}.pth'
-    weights_file = Path(f'./{model_id}.pth')
+    weights_file = Path(f'{env.Env.get().tmp_dir}/{model_id}.pth')
     if not weights_file.exists():
         print(f'Downloading weights for YOLOX model {model_id}')
         print(f'Downloading from {weights_url} to {weights_file}')
@@ -41,18 +48,15 @@ def __lookup_model(model_id: str) -> (YOLOX, Exp):
     exp = get_exp(exp_name=model_id)
     model = exp.get_model()
 
-    # Inference on gpu
-    # model.cuda()
-
     model.eval()
     model.head.training = False
     model.training = False
 
     # Load in the weights from training
-    weights = torch.load(weights_file, map_location=torch.device('cpu'))
+    weights = torch.load(weights_file, map_location=torch.device(device))
     model.load_state_dict(weights['model'])
 
-    __model_cache[model_id] = (model, exp)
+    __model_cache[key] = (model, exp)
     return model, exp
 
 
