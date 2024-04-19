1ddd6d1cf38e584246baa90bcb4d28dd4285b45c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-04-18 20:55:09 -0700

    More progress on yolox adapter

--- pixeltable/functions/yolox.py
+++ pixeltable/functions/yolox.py
@@ -12,33 +12,33 @@ from yolox.utils import postprocess
 from pixeltable import env
 
 
-def yolox(img: PIL.Image.Image, *, model_id: str, device: str = 'cpu') -> dict:
-    model, exp = get_model(model_id)
-    val_transform = ValTransform(legacy=False)
+def yolox(image: PIL.Image.Image, *, model_id: str, device: str = 'cpu') -> dict:
+    model, exp = __lookup_model(model_id)
 
-    img, _ = val_transform(np.array(img), None, exp.test_size)
-    img = torch.from_numpy(img).unsqueeze(0).float()
-    if device == "gpu":
-        img = img.cuda()
+    image_transform, _ = __val_transform(np.array(image), None, exp.test_size)
+    image_tensor = torch.from_numpy(image_transform).unsqueeze(0).float()
+    image_tensor.to(device)
 
     with torch.no_grad():
-        outputs = model(img)
+        outputs = model(image_tensor)
         outputs = postprocess(
-            outputs, 1, exp.test_conf,
-            exp.nmsthre, class_agnostic=True
+            outputs, 1, exp.test_conf, exp.nmsthre, class_agnostic=True
         )
     return outputs
 
 
-def get_model(name: str) -> (YOLOX, Exp):
-    weights_url = f'https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/{name}.pth'
-    weights_file = Path(f'{env.Env.get().tmp_dir}/{name}.pth')
+def __lookup_model(model_id: str) -> (YOLOX, Exp):
+    if model_id in __model_cache:
+        return __model_cache[model_id]
+
+    weights_url = f'https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/{model_id}.pth'
+    weights_file = Path(f'./{model_id}.pth')
     if not weights_file.exists():
-        print(f'Downloading weights for YOLOX model {name}')
+        print(f'Downloading weights for YOLOX model {model_id}')
         print(f'Downloading from {weights_url} to {weights_file}')
         urlretrieve(weights_url, weights_file)
 
-    exp = get_exp(exp_name=name)
+    exp = get_exp(exp_name=model_id)
     model = exp.get_model()
 
     # Inference on gpu
@@ -52,4 +52,9 @@ def get_model(name: str) -> (YOLOX, Exp):
     weights = torch.load(weights_file, map_location=torch.device('cpu'))
     model.load_state_dict(weights['model'])
 
+    __model_cache[model_id] = (model, exp)
     return model, exp
+
+
+__model_cache = {}
+__val_transform = ValTransform(legacy=False)
