a6ee01fbd8b23e772c561e86ebc8f90650e16042
Oscar Moll <orm@csail.mit.edu>
2024-05-03 11:37:50 -0400

    test file server

--- pixeltable/tests/test_http_server.py
+++ pixeltable/tests/test_http_server.py
@@ -1,5 +1,5 @@
 from pixeltable.utils.http_server import path_to_parts, get_file_uri, make_server
-from .utils import get_documents, get_video_files
+from .utils import get_documents, get_video_files, get_audio_files, get_image_files
 import urllib
 import threading
 
@@ -30,9 +30,15 @@ def test_http_server():
     thread = threading.Thread(target=run_server, daemon=True)
     thread.start()
 
-    for doc in get_documents():
-        uri = get_file_uri(http_address, doc)
+    for docucment_file in get_documents():
+        uri = get_file_uri(http_address, docucment_file)
         assert urllib.request.urlopen(uri).getcode() == 200
     for video_file in get_video_files():
         uri = get_file_uri(http_address, video_file)
+        assert urllib.request.urlopen(uri).getcode() == 200
+    for audio_file in get_audio_files():
+        uri = get_file_uri(http_address, audio_file)
+        assert urllib.request.urlopen(uri).getcode() == 200
+    for image_file in get_image_files()[:10]:
+        uri = get_file_uri(http_address, image_file)
         assert urllib.request.urlopen(uri).getcode() == 200
