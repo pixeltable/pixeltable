175a376a360f421aadb7d8c355901244ebf21f9f
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-05-15 11:11:10 -0700

    Use schema v15 for label studio

--- pixeltable/metadata/converters/convert_14.py
+++ pixeltable/metadata/converters/convert_14.py
@@ -4,10 +4,10 @@ from pixeltable.metadata.schema import Table
 from pixeltable.metadata import register_converter
 
 
-def convert_13(engine: sql.engine.Engine) -> None:
+def convert_14(engine: sql.engine.Engine) -> None:
     default_remotes = {'remotes': []}
     with engine.begin() as conn:
         conn.execute(sql.update(Table).where(Table.md['remotes'] == None).values(md=Table.md.concat(default_remotes)))
 
 
-register_converter(13, convert_13)
+register_converter(14, convert_14)
