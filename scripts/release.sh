978579d07a1394168ebf886f221080ba4faa487c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-05-16 19:48:48 -0700

    Updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,9 @@
 
 set -e
 
-if [ -z "$1" ]; then
-  echo "Usage: release.sh <pypi-api-key>"
+if [ -z "$2" ]; then
+  echo "Usage: release.sh <version> <pypi-api-key>"
+  echo "Example: release.sh 0.2.6 api-key"
   exit 1
 fi
 
@@ -15,23 +16,37 @@ if [ "$(git remote get-url origin)" != 'https://github.com/pixeltable/pixeltable
   exit 1
 fi
 
-PYPI_API_KEY="$1"
+VERSION="$1"
+PYPI_API_KEY="$2"
 SCRIPT_DIR="$(dirname "$0")"
 PROJECT_ROOT="$SCRIPT_DIR/.."
-VERSION="$(grep -o '^version = "[^"]*' "$PROJECT_ROOT/pyproject.toml" | sed 's/version = "//')"
 
-if [ -z "$VERSION" ]; then
-  echo "Could not extract version from pyproject.toml."
-  exit 1
-fi
+# VERSION="$(grep -o '^version = "[^"]*' "$PROJECT_ROOT/pyproject.toml" | sed 's/version = "//')"
+# if [ -z "$VERSION" ]; then
+#   echo "Could not extract version from pyproject.toml."
+#   exit 1
+# fi
 
 echo "Project root: $PROJECT_ROOT"
 echo "This will publish version $VERSION. Enter to confirm; Ctrl-C to abort."
 read
 
 cd "$PROJECT_ROOT"
-poetry build
-poetry publish --username __token__ --password "$PYPI_API_KEY"
 git tag "v$VERSION"
 git push origin "v$VERSION"
-# TODO: Also run mkdocs and create a release using github CLI
+
+echo "v$VERSION tag created and pushed to home repo."
+echo "Enter to proceed; Ctrl-C to abort."
+read
+
+make clean
+poetry build
+poetry publish --username __token__ --password "$PYPI_API_KEY"
+
+echo "Updating mkdocs ..."
+mkdocs build
+mkdocs gh-deploy
+
+echo "Creating release on github ..."
+gh release create v$VERSION --generate-notes
+open https://github.com/pixeltable/pixeltable/releases/tag/v$VERSION
