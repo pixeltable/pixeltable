7594ad1386752310f8267682e3651c55f54181d8
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-05-19 10:49:57 -0700

    Require clean repo to run release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,15 +2,23 @@
 
 # Pixeltable release script
 
+SCRIPT_DIR="$(dirname "$0")"
+PROJECT_ROOT="$(readlink -f "$SCRIPT_DIR/..")"
+
+echo -e "\n============= Running pixeltable release script.\n"
+
+echo "Project root: $PROJECT_ROOT"
+cd "$PROJECT_ROOT"
+
 if [ "$(git remote get-url home)" != 'https://github.com/pixeltable/pixeltable' ]; then
   echo "Unexpected home repo: $(git remote get-url home)"
   exit 1
 fi
 
-SCRIPT_DIR="$(dirname "$0")"
-PROJECT_ROOT="$(readlink -f "$SCRIPT_DIR/..")"
-
-echo "Project root: $PROJECT_ROOT"
+if [[ -n "$(git diff-index HEAD)" || -n "$(git diff-files)" || -n "$(git ls-files --exclude-standard --others)" ]]; then
+  echo "The release script must be run from a clean git repo."
+  exit 1
+fi
 
 if [ -z "$PYPI_API_KEY" ]; then
   echo "PYPI_API_KEY not defined in environment; searching in ~/.pixeltable/config.yaml."
@@ -26,7 +34,6 @@ echo ""
 echo "This will publish version v$VERSION to PyPI. Enter to confirm; Ctrl-C to abort."
 read
 
-cd "$PROJECT_ROOT"
 git tag "v$VERSION"
 git push home "v$VERSION"
 
@@ -39,5 +46,5 @@ poetry build
 poetry publish --username __token__ --password "$PYPI_API_KEY"
 
 echo "Creating release on github ..."
-gh release create v$VERSION --generate-notes
-open https://github.com/pixeltable/pixeltable/releases/tag/v$VERSION
+gh release create "v$VERSION" --generate-notes
+open "https://github.com/pixeltable/pixeltable/releases/tag/v$VERSION"
