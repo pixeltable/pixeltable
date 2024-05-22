156e1bab2cc81eb3329dc56a235ee0b60bf19513
Oscar Moll <orm@csail.mit.edu>
2024-05-21 21:26:14 -0400

    add new methods

--- pixeltable/utils/formatter.py
+++ pixeltable/utils/formatter.py
@@ -5,12 +5,20 @@ import PIL.Image as Image
 import io
 import cv2
 import mimetypes
-from typing import Any
+from typing import Any, Optional
+import json
 
 from pixeltable.utils.http_server import get_file_uri
 import logging
 
 _logger = logging.getLogger('pixeltable')
+
+FLOAT_PRECISION = 3
+NP_THRESHOLD = 16
+NP_EDGEITEMS = 6
+STRING_THRESHOLD = 250
+STRING_EDGEITEMS = 120
+INDENT = 1
 
 def _create_source_tag(http_address: str, file_path: str) -> str:
     src_url = get_file_uri(http_address, file_path)
@@ -19,11 +27,120 @@ def _create_source_tag(http_address: str, file_path: str) -> str:
     mime_attr = f'type="{mime}"' if mime is not None else ''
     return f'<source src="{src_url}" {mime_attr} />'
 
+
+def escape_string_for_json(input_string: str) -> str:
+    """ the output here can be printed to screen, and copy-pasted, to get the same string value """
+    # Use encode to escape special characters
+    escaped_string = input_string.encode('unicode_escape').decode('utf-8')
+    # Escape double quotes
+    escaped_string = escaped_string.replace('"', '\\"')
+    return escaped_string
+
+
+def as_simple_ndarray(val: list[Any]) -> Optional[np.ndarray]:
+    """attempts to parse list as a numerical np.ndarray, if not possible returns None"""
+    arr = None
+    try:
+        arr = np.array(val)
+    except TypeError:
+        return None
+
+    if not np.issubdtype(arr.dtype, np.number):
+        arr = None
+
+    return arr
+
+
+def is_simple_array(val: list[Any]) -> bool:
+    for v in val:
+        if isinstance(v,list) or isinstance(v,dict):
+            return False
+    return True
+
+FLOAT_PRECISION = 3
+NP_THRESHOLD = 16
+NP_EDGEITEMS = 6
+STRING_THRESHOLD = 250
+STRING_EDGEITEMS = 120
+INDENT = 1
+
+def escape_string_for_json(input_string):
+    """ the output here can be printed to screen, and copy-pasted, to get the same string value """
+    # Use encode to escape special characters
+    escaped_string = input_string.encode('unicode_escape').decode('utf-8')
+    # Escape double quotes
+    escaped_string = escaped_string.replace('"', '\\"')
+    return escaped_string
+
+
 class PixeltableFormatter:
     def __init__(self, num_rows: int, num_cols: int, http_address: str):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.http_address = http_address
+
+    def _format_float(self, val: float) -> str:
+        # stay consistent with numpy formatting (0-D array has no brackets)
+        return np.array2string(np.array(val), precision=FLOAT_PRECISION)
+
+    def _format_array(self, arr: list[Any]) -> str:
+        return np.array2string(arr, precision=FLOAT_PRECISION, threshold=NP_THRESHOLD, separator=',', edgeitems=NP_EDGEITEMS)
+
+    def _format_string(self, val: str) -> str:
+        if len(val) > STRING_THRESHOLD:
+            return f'"{escape_string_for_json(val[:STRING_EDGEITEMS])} ...... {escape_string_for_json(val[-STRING_EDGEITEMS:])}"'
+        return f'"{escape_string_for_json(val)}"'
+
+    def _format_json_helper(self, obj: Any, level: int) -> str:
+        def get_prefix(level):
+            return ' ' * (level * INDENT)
+
+        if isinstance(obj, list):
+            # we will distinguish 3 separate cases:
+            # 1. numerical arrays (potentially multiple levels of nesting, often seen as model outputs)
+            #   => treat just like array columns for consistency.
+            # 2. list of elementary types (anything elementary not covered by the above, like strings)
+            #   => (eg category names) print in one line (ie, avoid line per element)
+            # 3. list with some complex types, eg list of dicts.
+            #   => insert newlines between elements to help read them.
+            arr = as_simple_ndarray(obj)
+            if arr is not None:
+                arrstr = self._format_array(arr)
+                return '\n'.join([f'{get_prefix(level)}{line}' for line in arrstr.splitlines()])
+            out_pieces = []
+            if is_simple_array(obj):
+                for elt in obj:
+                    fmt_elt = self._format_json_helper(elt, level=0)
+                    out_pieces.append(fmt_elt)
+                contents = ','.join(out_pieces)
+                return f'{get_prefix(level)}[{contents}]'
+            else:
+                for elt in obj:
+                    fmt_elt = self._format_json_helper(elt, level=level+1)
+                    out_pieces.append(fmt_elt)
+                joiner = ',\n'
+                contents = joiner.join(out_pieces)
+                return f'{get_prefix(level)}[\n{contents}\n{get_prefix(level)}]'
+
+        elif isinstance(obj, dict):
+            out_pieces = []
+            for key, value in obj.items():
+                fmt_value = self._format_json_helper(value, level=level+1)
+                fmt_key = self._format_string(key)
+                out_pieces.append(f'{get_prefix(level+1)}{fmt_key}: {fmt_value.lstrip()}')
+            contents = ',\n'.join(out_pieces)
+            return f'{get_prefix(level)}{{\n{contents}\n{get_prefix(level)}}}'
+        elif isinstance(obj, float):
+            return self._format_float(obj)
+        elif isinstance(obj, str):
+            return self._format_string(obj)
+        else:
+            # follow same convention as json.dumps for other elementary types
+            # eg. int, None
+            return json.dumps(obj)
+
+    def _format_json(self, obj: Any) -> str:
+        return self._format_json_helper(obj, level=0)
 
     def _format_img(self, img: Image.Image) -> str:
         """
@@ -77,18 +194,6 @@ class PixeltableFormatter:
             </video>
         </div>
         """
-
-    def _format_array(self, arr: list[Any]) -> str:
-        arr = np.array(arr)
-        return np.array2string(arr, precision=3, threshold=16, separator=',', edgeitems=6)
-
-    def _format_string(self, string: str) -> str:
-        if len(string) > 250:
-            return f'{string[:120]} ...... {string[-120:]}'
-        return string
-
-    def _format_json(self, obj: Any) -> str:
-        pass
 
     def _format_document(self, file_path: str) -> str:
         max_width = max_height = 320
