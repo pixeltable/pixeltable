c15be33c9c57338e4d7ed88bf125432103990d9b
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-05-28 13:36:26 -0700

    Reverse test operations (for better error messages)

--- tests/ext/test_whisperx.py
+++ tests/ext/test_whisperx.py
@@ -10,8 +10,8 @@ class TestWhisperx:
 
         audio_file = next(file for file in get_audio_files() if file.endswith('jfk_1961_0109_cityuponahill-excerpt.flac'))
         t = pxt.create_table('whisperx', {'audio': pxt.AudioType()})
-        validate_update_status(t.insert(audio=audio_file), expected_rows=1)
         t['transcription'] = whisperx.transcribe(t.audio, model='large-v2')
+        validate_update_status(t.insert(audio=audio_file), expected_rows=1)
         result = t.collect()['transcription'][0]
         assert result['language'] == 'en'
         assert 'city upon a hill' in result['segments'][1]['text']
