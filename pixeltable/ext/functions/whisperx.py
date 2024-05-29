ce3045b1797c57946145103616229c3ee3ca36f2
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-05-28 22:36:29 -0700

    Skip whisperx tests on intel macs

--- pixeltable/ext/functions/whisperx.py
+++ pixeltable/ext/functions/whisperx.py
@@ -5,17 +5,11 @@ import whisperx
 from whisperx.asr import FasterWhisperPipeline
 
 import pixeltable as pxt
-from pixeltable import udf
 
 
-@udf(param_types=[pxt.AudioType(), pxt.StringType(), pxt.StringType(), pxt.StringType(), pxt.IntType()])
+@pxt.udf(param_types=[pxt.AudioType(), pxt.StringType(), pxt.StringType(), pxt.StringType(), pxt.IntType()])
 def transcribe(
-        audio: str,
-        *,
-        model: str,
-        compute_type: Optional[str] = None,
-        language: Optional[str] = None,
-        chunk_size: int = 30
+    audio: str, *, model: str, compute_type: Optional[str] = None, language: Optional[str] = None, chunk_size: int = 30
 ) -> dict:
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     compute_type = compute_type or ('float16' if device == 'cuda' else 'int8')
