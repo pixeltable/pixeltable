28d5689ba3029128af6eff5857a1184bc62c72eb
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-06-14 15:35:49 -0700

    Additional coverage in db dumps

---
+++ pixeltable/tool/embed_udf.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import pixeltable as pxt
+
+
+# TODO This can go away once we have the ability to inline expr_udf's
+@pxt.expr_udf
+def clip_text_embed(txt: str) -> np.ndarray:
+    return pxt.functions.huggingface.clip_text(txt, model_id='openai/clip-vit-base-patch32')
