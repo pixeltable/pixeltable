05efd499efcdfe03eb7adf715ced1f275e94a4bb
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-06-24 16:12:40 -0700

    Update tutorial

--- docs/release/tutorials/embedding.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pixeltable as pxt
-from pixeltable.functions.huggingface import sentence_transformer
-
-@pxt.expr_udf
-def e5_embed(text: str) -> np.ndarray:
-    return sentence_transformer(text, model_id='intfloat/e5-large-v2')
-
