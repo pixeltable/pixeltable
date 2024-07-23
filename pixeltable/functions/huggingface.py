874b7bd7e5225d54bf7910f52f4694c878a3e27f
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-07-22 21:42:35 -0700

    More doc examples

--- pixeltable/functions/huggingface.py
+++ pixeltable/functions/huggingface.py
@@ -25,15 +25,16 @@ def sentence_transformer(
 ) -> Batch[np.ndarray]:
     """
     Runs the specified pretrained sentence-transformers model. `model_id` should be a pretrained model, as described
-    in the [Sentence Transformers Pretrained Models](https://sbert.net/docs/sentence_transformer/pretrained_models.html) doc.
+    in the [Sentence Transformers Pretrained Models](https://sbert.net/docs/sentence_transformer/pretrained_models.html)
+    documentation.
 
     __Requirements:__
 
     - `pip install sentence-transformers`
 
-    Parameters:
-        sentence: The sentence to embed
-        model_id: The pretrained model to use for the encoding
+    Args:
+        sentence: The sentence to embed.
+        model_id: The pretrained model to use for the encoding.
         normalize_embeddings: If `True`, normalizes embeddings to length 1; see the
             [Sentence Transformers API Docs](https://sbert.net/docs/package_reference/sentence_transformer/SentenceTransformer.html)
             for more details
@@ -80,7 +81,32 @@ def sentence_transformer_list(sentences: list, *, model_id: str, normalize_embed
 
 @pxt.udf(batch_size=32)
 def cross_encoder(sentences1: Batch[str], sentences2: Batch[str], *, model_id: str) -> Batch[float]:
-    """Runs the specified cross-encoder model."""
+    """
+    Runs the specified cross-encoder model to compute similarity scores for pairs of sentences.
+    `model_id` should be a pretrained model, as described in the
+    [Cross-Encoder Pretrained Models](https://www.sbert.net/docs/cross_encoder/pretrained_models.html)
+    documentation.
+
+    __Requirements:__
+
+    - `pip install sentence-transformers`
+
+    Parameters:
+        sentences1: The first sentence to be paired.
+        sentences2: The second sentence to be paired.
+        model_id: The identifier of the cross-encoder model to use.
+
+    Returns:
+        The similarity score between the inputs.
+
+    Examples:
+        Add a computed column that applies the model `ms-marco-MiniLM-L-4-v2` to the sentences in
+        columns `tbl.sentence1` and `tbl.sentence2`:
+
+        >>> tbl['result'] = sentence_transformer(
+                tbl.sentence1, tbl.sentence2, model_id='ms-marco-MiniLM-L-4-v2'
+            )
+    """
     env.Env.get().require_package('sentence_transformers')
     from sentence_transformers import CrossEncoder
 
