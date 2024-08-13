363efc2a510b8a0e2135e124544555efd32a089b
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-08-13 11:09:25 -0700

    Add a new DB version to capture UDF renames and ImageMemberAccess removal

---
+++ pixeltable/metadata/notes.py
@@ -0,0 +1,7 @@
+version_notes = {
+    15: 'Remotes in table metadata',
+    16: 'Query functions; deferred Expr deserialization',
+    17: 'Renamed remotes to external_stores',
+    18: 'Restructured index metadata',
+    19: 'UDF renames; ImageMemberAccess removal',
+}
