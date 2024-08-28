f952e917cd327b96abb27e732d027c986bac47d8
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-08-28 10:20:52 -0700

    mypy plugin cleanup

--- pixeltable/tool/mypy_plugin.py
+++ pixeltable/tool/mypy_plugin.py
@@ -1,18 +1,19 @@
 from typing import Callable, Optional
 
-from mypy.plugin import AnalyzeTypeContext, DynamicClassDefContext, FunctionSigContext, Plugin
-from mypy.types import FunctionLike, Type
+from mypy.plugin import AnalyzeTypeContext, Plugin
+from mypy.types import Type
 
 
 class PxtPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str) -> Optional[Callable[[AnalyzeTypeContext], type]]:
-        if fullname in ('pixeltable.type_system.ImageT', 'pixeltable.type_system.ArrayT'):
-            return pxt_hook
+        if fullname == 'pixeltable.type_system.ImageT':
+            return lambda ctx: pxt_hook(ctx, 'PIL.Image.Image')
+        if fullname == 'pixeltable.type_system.ArrayT':
+            return lambda ctx: pxt_hook(ctx, 'numpy.ndarray')
         return None
 
 def plugin(version: str):
     return PxtPlugin
 
-def pxt_hook(ctx: AnalyzeTypeContext) -> Type:
-    print(ctx.type.name)
-    return ctx.api.named_type('PIL.Image.Image')
+def pxt_hook(ctx: AnalyzeTypeContext, subst_name: str) -> Type:
+    return ctx.api.named_type(subst_name)
