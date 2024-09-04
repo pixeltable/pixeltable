e6b66d602bd97ac017972d73fbbe32d731b78190
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-09-03 22:44:55 -0700

    Disable test_db_dump_tool on Windows

--- tests/tool/test_db_dump_tool.py
+++ tests/tool/test_db_dump_tool.py
@@ -1,8 +1,12 @@
+import platform
 import subprocess
+
+import pytest
 
 from ..utils import skip_test_if_not_installed
 
 
+@pytest.mark.skipif(platform.system() == 'Windows', reason='Tool is not supported on Windows')
 class TestDbDumpTool:
 
     def test_db_dump_tool(self) -> None:
