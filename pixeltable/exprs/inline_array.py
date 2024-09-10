34fe4be36ab09ef3df5bd8a36f3e5c703e8a0876
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2024-09-10 11:02:26 -0700

    Cleanup InlineArray

--- pixeltable/exprs/inline_array.py
+++ pixeltable/exprs/inline_array.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 import copy
-from typing import Iterable, Optional, List, Any, Dict, Tuple
+from typing import Any, Dict, Iterable, List, Optional
 
 import numpy as np
 import sqlalchemy as sql
 
+import pixeltable.exceptions as excs
 import pixeltable.type_system as ts
+
 from .data_row import DataRow
 from .expr import Expr
-from .inline_dict import InlineDict
 from .row_builder import RowBuilder
 
 
@@ -22,9 +23,9 @@ class InlineArray(Expr):
     `ArrayType` if it is a homogenous array of scalars or arrays, or a `JsonType` otherwise.
     """
 
-    elements: List[Tuple[Optional[int], Any]]
+    elements: list[tuple[Optional[int], Any]]
 
-    def __init__(self, elements: Iterable, force_json: bool = False):
+    def __init__(self, elements: Iterable):
         # we need to call this in order to populate self.components
         super().__init__(ts.ArrayType((len(elements),), ts.IntType()))
 
@@ -33,13 +34,8 @@ class InlineArray(Expr):
         # - for non-Expr elements: (None, value)
         self.elements = []
         for el in elements:
-            el = copy.deepcopy(el)
             if isinstance(el, list):
-                # If col_type is an ArrayType, we'll require it to be a multidimensional array
-                # of the specified underlying type
-                el = InlineArray(tuple(el), force_json)
-            if isinstance(el, dict):
-                el = InlineDict(el)
+                el = InlineArray(el)
             if isinstance(el, Expr):
                 self.elements.append((len(self.components), None))
                 self.components.append(el)
@@ -47,28 +43,22 @@ class InlineArray(Expr):
                 self.elements.append((None, el))
 
         inferred_element_type: Optional[ts.ColumnType] = ts.InvalidType()
-        if not force_json:
-            # try to infer the element type
-            for idx, val in self.elements:
-                if idx is not None:
-                    inferred_element_type = inferred_element_type.supertype(self.components[idx].col_type)
-                else:
-                    inferred_element_type = inferred_element_type.supertype(ts.ColumnType.infer_literal_type(val))
-                if inferred_element_type is None:
-                    break
+        for idx, val in self.elements:
+            if idx is not None:
+                inferred_element_type = inferred_element_type.supertype(self.components[idx].col_type)
+            else:
+                inferred_element_type = inferred_element_type.supertype(ts.ColumnType.infer_literal_type(val))
+            if inferred_element_type is None:
+                raise excs.Error('Could not infer element type of array')
 
-        if force_json or inferred_element_type is None:
-            # JSON conversion is forced, or there is no common supertype
-            # TODO: make sure this doesn't contain Images
-            self.col_type = ts.JsonType()
-        elif inferred_element_type.is_scalar_type():
+        if inferred_element_type.is_scalar_type():
             self.col_type = ts.ArrayType((len(self.elements),), inferred_element_type)
         elif inferred_element_type.is_array_type():
             assert isinstance(inferred_element_type, ts.ArrayType)
             self.col_type = ts.ArrayType(
                 (len(self.elements), *inferred_element_type.shape), ts.ColumnType.make_type(inferred_element_type.dtype))
         else:
-            self.col_type = ts.JsonType()
+            raise excs.Error(f'Element type is not a valid dtype for an array: {inferred_element_type}')
 
         self.id = self._create_id()
 
@@ -76,13 +66,13 @@ class InlineArray(Expr):
         elem_strs = [str(val) if val is not None else str(self.components[idx]) for idx, val in self.elements]
         return f'[{", ".join(elem_strs)}]'
 
-    def _equals(self, other: InlineDict) -> bool:
+    def _equals(self, other: InlineArray) -> bool:
         return self.elements == other.elements
 
-    def _id_attrs(self) -> List[Tuple[str, Any]]:
+    def _id_attrs(self) -> list[tuple[str, Any]]:
         return super()._id_attrs() + [('elements', self.elements)]
 
-    def sql_expr(self) -> Optional[sql.ClauseElement]:
+    def sql_expr(self) -> Optional[sql.ColumnElement]:
         return None
 
     def eval(self, data_row: DataRow, row_builder: RowBuilder) -> None:
@@ -98,12 +88,12 @@ class InlineArray(Expr):
             data_row[self.slot_idx] = result
 
     def _as_dict(self) -> Dict:
-        return {'elements': self.elements, 'is_json': self.col_type.is_json_type(), **super()._as_dict()}
+        return {'elements': self.elements, **super()._as_dict()}
 
     @classmethod
     def _from_dict(cls, d: Dict, components: List[Expr]) -> Expr:
         assert 'elements' in d
-        arg: List[Any] = []
+        arg: list[Any] = []
         for idx, val in d['elements']:
             # TODO Normalize idx -1 to None via schema migrations.
             # Long-term we should not be allowing idx == -1.
@@ -111,6 +101,4 @@ class InlineArray(Expr):
                 arg.append(components[idx])
             else:
                 arg.append(val)
-        # in order to avoid a schema version change, we'll interpret the absence of 'is_json' to indicate an ArrayType
-        is_json = d.get('is_json', False)
-        return cls(tuple(arg), force_json=is_json)
+        return cls(tuple(arg))
