50c9a9b7d2fc3ee3063a7253f1c4bd11adf59237
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-01-23 10:09:21 -0800

    Rename methods in mypy plugin and add docstrings

--- tool/mypy_plugin.py
+++ tool/mypy_plugin.py
@@ -34,17 +34,17 @@ class PxtPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str) -> Optional[Callable[[AnalyzeTypeContext], Type]]:
         if fullname in self.__FULLNAME_MAP:
             subst_name = self.__FULLNAME_MAP[fullname]
-            return lambda ctx: pxt_hook(ctx, subst_name)
+            return lambda ctx: adjust_pxt_type(ctx, subst_name)
         return None
 
     def get_method_signature_hook(self, fullname: str) -> Optional[Callable[[MethodSigContext], FunctionLike]]:
         if fullname in [self.__ADD_COLUMN_FULLNAME, self.__ADD_COMPUTED_COLUMN_FULLNAME]:
-            return pxt_method_hook
+            return adjust_kwargs
         return None
 
     def get_class_decorator_hook_2(self, fullname: str) -> Optional[Callable[[ClassDefContext], bool]]:
         if fullname == self.__UDA_FULLNAME:
-            return pxt_decorator_hook
+            return adjust_uda_methods
         return None
 
 def plugin(version: str) -> type:
@@ -54,9 +54,11 @@ _AGGREGATOR_FULLNAME = f'{pxt.Aggregator.__module__}.{pxt.Aggregator.__name__}'
 _FN_CALL_FULLNAME = f'{pxt.exprs.Expr.__module__}.{pxt.exprs.Expr.__name__}'
 
 def adjust_uda_type(ctx: FunctionContext) -> Type:
-    # Mypy doesn't understand that a class with a @uda decorator isn't actually a class, so it assumes
-    # that sum(expr), for example, actually returns an instance of sum. We correct this by changing the
-    # return type of any subclass of `Aggregator` to `FunctionCall`.
+    """
+    Mypy doesn't understand that a class with a @uda decorator isn't actually a class, so it assumes
+    that sum(expr), for example, actually returns an instance of sum. We correct this by changing the
+    return type of any subclass of `Aggregator` to `FunctionCall`.
+    """
     ret_type = ctx.default_return_type
     if isinstance(ret_type, Instance):
         if (
@@ -66,19 +68,43 @@ def adjust_uda_type(ctx: FunctionContext) -> Type:
             ret_type = AnyType(TypeOfAny.special_form)
     return ret_type
 
-def pxt_hook(ctx: AnalyzeTypeContext, subst_name: str) -> Type:
+def adjust_pxt_type(ctx: AnalyzeTypeContext, subst_name: str) -> Type:
+    """
+    Replaces the special Pixeltable classes (such as pxt.Array) with their standard equivalents (such as np.ndarray).
+    """
     if subst_name == 'typing.Any':
         return AnyType(TypeOfAny.special_form)
     return ctx.api.named_type(subst_name, [])
 
-def pxt_method_hook(ctx: MethodSigContext) -> FunctionLike:
+def adjust_kwargs(ctx: MethodSigContext) -> FunctionLike:
+    """
+    Mypy has a "feature" where it will spit out multiple warnings if a method with signature
+    ```
+    def my_func(*, arg1: int, arg2: str, **kwargs: Expr)
+    ```
+    (for example) is called with bare kwargs:
+    ```
+    my_func(my_kwarg=value)
+    ```
+    This is a disaster for type-checking of add_column and add_computed_column. Here we adjust the signature so
+    that mypy thinks it is simply
+    ```
+    def my_func(**kwargs: Any)
+    ```
+    thereby avoiding any type-checking errors. For details, see: <https://github.com/python/mypy/issues/18481>
+    """
     sig = ctx.default_signature
     new_arg_names = sig.arg_names[-1:]
     new_arg_types = [AnyType(TypeOfAny.special_form)]
     new_arg_kinds = sig.arg_kinds[-1:]
     return sig.copy_modified(arg_names=new_arg_names, arg_types=new_arg_types, arg_kinds=new_arg_kinds)
 
-def pxt_decorator_hook(ctx: ClassDefContext) -> bool:
+def adjust_uda_methods(ctx: ClassDefContext) -> bool:
+    """
+    Mypy does not handle the `@pxt.uda` aggregator well; it continues to treat the decorated class as a class,
+    even though it has been replaced by an `AggregateFunction`. Here we add static methods to the class that
+    imitate various (instance) methods of `AggregateFunction` so that they can be properly type-checked.
+    """
     list_type = ctx.api.named_type('builtins.list', [AnyType(TypeOfAny.special_form)])
     fn_arg = nodes.Argument(nodes.Var('fn'), AnyType(TypeOfAny.special_form), None, nodes.ARG_POS)
     args_arg = nodes.Argument(nodes.Var('args'), AnyType(TypeOfAny.special_form), None, nodes.ARG_STAR)
