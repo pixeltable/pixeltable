c5c6a52795d1701503a845409d88fcef2c201cd0
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-03-07 17:14:43 -0800

    Update Anthropic tests

--- tests/functions/test_anthropic.py
+++ tests/functions/test_anthropic.py
@@ -91,45 +91,45 @@ class TestAnthropic:
             # Request for stock price: works except when tool_choice is set explicitly to weather
             print('Checking stock price inquiry')
             if tool_choice is None or tool_choice.tool != 'weather':
-                assert res[0]['tool_calls'] == {'stock_price': 131.17, 'weather': None}
+                assert res[0]['tool_calls'] == {'stock_price': [131.17], 'weather': None}
             else:  # Explicitly set to weather; we may or may not get stock price also
                 assert res[0]['tool_calls'] in [
-                    {'stock_price': None, 'weather': 'Unknown city'},
-                    {'stock_price': 131.17, 'weather': 'Unknown city'},
+                    {'stock_price': None, 'weather': ['Unknown city']},
+                    {'stock_price': [131.17], 'weather': ['Unknown city']},
                 ]
 
             # Request for weather: works except when tool_choice is set explicitly to stock_price
             print('Checking weather inquiry')
             if tool_choice is None or tool_choice.tool != 'stock_price':
-                assert res[1]['tool_calls'] == {'stock_price': None, 'weather': 'Cloudy with a chance of meatballs'}
+                assert res[1]['tool_calls'] == {'stock_price': None, 'weather': ['Cloudy with a chance of meatballs']}
             else:  # Explicitly set to stock_price; we may or may not get weather also
                 assert res[1]['tool_calls'] in [
-                    {'stock_price': 0.0, 'weather': None},
-                    {'stock_price': 0.0, 'weather': 'Cloudy with a chance of meatballs'},
+                    {'stock_price': [0.0], 'weather': None},
+                    {'stock_price': [0.0], 'weather': ['Cloudy with a chance of meatballs']},
                 ]
 
             # Request for both stock price and weather
             print('Checking double inquiry')
             if tool_choice is None or tool_choice.parallel_tool_calls:
                 # Both tools invoked in parallel
-                assert res[2]['tool_calls'] == {'stock_price': 131.17, 'weather': 'Cloudy with a chance of meatballs'}
+                assert res[2]['tool_calls'] == {'stock_price': [131.17], 'weather': ['Cloudy with a chance of meatballs']}
             else:
                 # Only one tool invoked, but it's not specified which
                 assert not tool_choice.parallel_tool_calls
                 assert res[2]['tool_calls'] in [
-                    {'stock_price': 131.17, 'weather': None},
-                    {'stock_price': None, 'weather': 'Cloudy with a chance of meatballs'},
+                    {'stock_price': [131.17], 'weather': None},
+                    {'stock_price': None, 'weather': ['Cloudy with a chance of meatballs']},
                 ]
 
             print('Checking random question')
             if tool_choice is None or tool_choice.auto:
                 assert res[3]['tool_calls'] == {'stock_price': None, 'weather': None}
             elif tool_choice.tool == 'stock_price':
-                assert res[3]['tool_calls'] == {'stock_price': 0.0, 'weather': None}
+                assert res[3]['tool_calls'] == {'stock_price': [0.0], 'weather': None}
             elif tool_choice.tool == 'weather':
-                assert res[3]['tool_calls'] == {'stock_price': None, 'weather': 'Unknown city'}
+                assert res[3]['tool_calls'] == {'stock_price': None, 'weather': ['Unknown city']}
             else:
                 assert res[3]['tool_calls'] in [
-                    {'stock_price': 0.0, 'weather': None},
-                    {'stock_price': None, 'weather': 'Unknown city'},
+                    {'stock_price': [0.0], 'weather': None},
+                    {'stock_price': None, 'weather': ['Unknown city']},
                 ]
