9a4c28e6080f15bcdac8ee42f9588d369d32843f
jpeterson-pxt <jpeterson@pixeltable.com>
2025-06-02 13:27:31 -0700

    Fixup for conversion to version 37.

--- pixeltable/metadata/notes.py
+++ pixeltable/metadata/notes.py
@@ -2,6 +2,7 @@
 # rather than as a comment, so that the existence of a description can be enforced by
 # the unit tests when new versions are added.
 VERSION_NOTES = {
+    37: 'Add support for the sample() method on DataFrames',
     36: 'Added Table.lock_dummy',
     35: 'Track reference_tbl in ColumnRef',
     34: 'Set default value for is_pk field in column metadata to False',
