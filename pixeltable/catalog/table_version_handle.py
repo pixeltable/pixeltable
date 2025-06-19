fb221745ed2c77e82ad1cc844b7245112dc8cbf8
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-06-19 13:18:00 -0700

    Address review feedback

--- pixeltable/catalog/table_version_handle.py
+++ pixeltable/catalog/table_version_handle.py
@@ -79,9 +79,10 @@ class ColumnHandle:
 
     def get(self) -> 'Column':
         if self.col_id not in self.tbl_version.get().cols_by_id:
+            schema_version_drop = self.tbl_version.get()._tbl_md.column_md[self.col_id].schema_version_drop
             raise excs.Error(
-                f'Column has been dropped (no record for column ID {self.col_id} '
-                f'in table {self.tbl_version.get().versioned_name!r})'
+                f'Column has been dropped (no record for column ID {self.col_id} in table '
+                f'{self.tbl_version.get().versioned_name!r}; it was dropped in table version {schema_version_drop})'
             )
         return self.tbl_version.get().cols_by_id[self.col_id]
 
