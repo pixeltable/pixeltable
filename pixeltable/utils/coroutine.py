07701f23ccfea32dc56e31ec1fa4fbd39b927680
Marcel Kornacker <marcelk@gmail.com>
2025-07-01 16:57:35 -0700

    Handle 429 errors in RateLimitScheduler (#670)
    I verified by hand that this picks up on 429 errors generated by the
    gemini endpoints and also records non-retryable errors.
    
    We should figure out how to create tests for these scenarios.

--- pixeltable/utils/coroutine.py
+++ pixeltable/utils/coroutine.py
@@ -1,10 +1,10 @@
 import asyncio
 import threading
-from concurrent.futures import ThreadPoolExecutor
 from typing import Any, Coroutine, TypeVar
 
-T = TypeVar('T')
+from pixeltable.env import Env
 
+T = TypeVar('T')
 
 # TODO This is a temporary hack to be able to run async UDFs in contexts that are not properly handled by the existing
 #   scheduler logic (e.g., as an embedding function as part of a similarity lookup). Once the scheduler is fully
@@ -15,27 +15,10 @@ def run_coroutine_synchronously(coroutine: Coroutine[Any, Any, T], timeout: floa
     """
     Runs the given coroutine synchronously, even if called in the context of a running event loop.
     """
-
-    def run_in_new_loop() -> T:
-        new_loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(new_loop)
-        try:
-            return new_loop.run_until_complete(coroutine)
-        finally:
-            new_loop.close()
-
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        # No event loop; just call `asyncio.run()`
-        return asyncio.run(coroutine)
+    loop = Env.get().event_loop
 
     if threading.current_thread() is threading.main_thread():
-        if not loop.is_running():
-            return loop.run_until_complete(coroutine)
-        else:
-            with ThreadPoolExecutor() as pool:
-                future = pool.submit(run_in_new_loop)
-                return future.result(timeout=timeout)
+        return loop.run_until_complete(coroutine)
     else:
-        return asyncio.run_coroutine_threadsafe(coroutine, loop).result()
+        # Not in main thread, use run_coroutine_threadsafe
+        return asyncio.run_coroutine_threadsafe(coroutine, loop).result(timeout)
