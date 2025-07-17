400acfc77e2a1983d82af6ce526866c0c5514f76
Mirko Kämpf <mirko.kaempf@gmail.com>
2025-07-17 17:18:03 +0200

    added dpi and format parameters with defaults

--- pixeltable/iterators/pdf_page_extractor.py
+++ pixeltable/iterators/pdf_page_extractor.py
@@ -18,15 +18,21 @@ class PageChunk:
 
 
 class PdfPageExtractor(ComponentIterator):
-    def __init__(self, document: str):
+    def __init__(self, document: str, dpi: int = 300, image_format: str = 'png'):
         self._doc_handle = get_document_handle(document)
         assert self._doc_handle.pdf_doc is not None
         self._pdf_doc = self._doc_handle.pdf_doc
         self._page_iter = iter(self._pdf_doc)
+        self._dpi = dpi
+        self._image_format = image_format
 
     @classmethod
     def input_schema(cls) -> dict[str, ColumnType]:
-        return {'document': StringType(nullable=False)}
+        return {
+            'document': StringType(nullable=False),
+            'dpi': IntType(nullable=True),
+            'image_format': StringType(nullable=True),
+        }
 
     @classmethod
     def output_schema(cls, *args: Any, **kwargs: Any) -> tuple[dict[str, ColumnType], list[str]]:
@@ -45,8 +51,8 @@ class PdfPageExtractor(ComponentIterator):
         clean_text = text.replace('\x00', '')
 
         # Render image
-        pix = page.get_pixmap(dpi=300)
-        image = Image.open(io.BytesIO(pix.tobytes('png')))
+        pix = page.get_pixmap(dpi=self._dpi)
+        image = Image.open(io.BytesIO(pix.tobytes(self._image_format)))
 
         return {'text': clean_text, 'page': page_number, 'image': image}
 
