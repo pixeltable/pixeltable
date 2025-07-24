7d68164dc0c317a1a93ea65b0f767ee3f2fd102f
jpeterson-pxt <jpeterson@pixeltable.com>
2025-07-24 09:36:46 -0700

    comments for PR media_8

--- pixeltable/exprs/data_row.py
+++ pixeltable/exprs/data_row.py
@@ -282,13 +282,14 @@ class DataRow:
         self.vals[index] = None
 
     def move_tmp_media_file(self, index: int, col: catalog.Column) -> None:
-        """Move a media file from a temporary location to the MediaStore"""
+        """If a media url refers to data in a temporary file, move the data to the MediaStore"""
         if self.file_urls[index] is None:
             return
         assert self.excs[index] is None
         assert col.col_type.is_media_type()
         src_path = MediaStore.resolve_tmp_url(self.file_urls[index])
         if src_path is None:
+            # The media url does not point to a temporary file, leave it as is
             return
         new_file_url = MediaStore.relocate_local_media_file(src_path, col)
         self.file_urls[index] = new_file_url
