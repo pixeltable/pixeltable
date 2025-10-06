494e8f39b8683d43a47b01411b79dfd53fadf2ec
jpeterson-pxt <jpeterson@pixeltable.com>
2025-10-06 12:03:00 -0700

    Enable removal of all local files from media store.

--- pixeltable/catalog/table_version.py
+++ pixeltable/catalog/table_version.py
@@ -372,7 +372,7 @@ class TableVersion:
     def delete_media(self, tbl_version: Optional[int] = None) -> None:
         # Assemble a set of column destinations and delete objects from all of them
         # None is a valid column destination which refers to the default object location
-        destinations = {col.destination for col in self.cols if col.is_stored and col.col_type.is_media_type()}
+        destinations = {col.destination for col in self.cols if col.is_stored and col.col_type.is_object_type()}
         for dest in destinations:
             ObjectOps.delete(dest, self.id, tbl_version=tbl_version)
 
