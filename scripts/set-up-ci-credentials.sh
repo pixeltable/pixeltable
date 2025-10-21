887f363cf5f2c690a903b8737090b197b9752a32
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-10-21 16:25:52 -0700

    Fixes

--- scripts/set-up-ci-credentials.sh
+++ scripts/set-up-ci-credentials.sh
@@ -10,26 +10,39 @@ region = ${AWS_DEFAULT_REGION}
 EOF
 
 # Add profiles to AWS credentials
+if [ -n "${AWS_ACCESS_KEY_ID}" ]; then
 cat >> ~/.aws/credentials << EOF
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 
+EOF
+fi
+
+if [ -n "${R2_ACCESS_KEY_ID}" ]; then
+cat >> ~/.aws/credentials << EOF
 [r2_profile]
 aws_access_key_id = ${R2_ACCESS_KEY_ID}
 aws_secret_access_key = ${R2_SECRET_ACCESS_KEY}
 
+EOF
+fi
+
+if [ -n "${B2_ACCESS_KEY_ID}" ]; then
+cat >> ~/.aws/credentials << EOF
 [b2_profile]
 aws_access_key_id = ${B2_ACCESS_KEY_ID}
 aws_secret_access_key = ${B2_SECRET_ACCESS_KEY}
+
 EOF
+fi
 
 # Set permissions
 chmod a+r ~/.aws/config
-chmod a+r ~/.aws/credentials
+chmod a+r ~/.aws/credentials || true
 
 # Setup GCS if credentials are provided
-if [ ! -z "${GCS_SERVICE_ACCOUNT_KEY}" ]; then
+if [ -n "${GCS_SERVICE_ACCOUNT_KEY}" ]; then
     echo "${GCS_SERVICE_ACCOUNT_KEY}" > /tmp/gcs-key.json
     chmod a+r /tmp/gcs-key.json
 fi
