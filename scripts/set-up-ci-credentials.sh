84fb4cac3f93a08fad75184d54cbf35688c2f54d
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-10-21 14:04:47 -0700

    Update CI

--- scripts/set-up-ci-credentials.sh
+++ scripts/set-up-ci-credentials.sh
@@ -1,37 +1,35 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Replacee existing ~/.aws config and credentials files with values needed to access an R2 storage destination.
-# Values are copied from the three environment variables:
-# R2_ACCESS_KEY_ID
-# R2_SECRET_ACCESS_KEY
-# so that the r2_profile can be used when setting up boto3, via the python code:
-#    session = boto3.Session(profile_name='r2_profile')
-
-# Create AWS directory if needed
-mkdir -p ~/.aws
-
-# Remove any existing config or credentials files
-rm -rf ~/.aws/config
-rm -rf ~/.aws/credentials
+# Create AWS directory (should be empty at time this script is run)
+mkdir ~/.aws
 
 # Add default to AWS config
-cat > ~/.aws/config << EOF
+cat >> ~/.aws/config << EOF
 [default]
+region = ${AWS_DEFAULT_REGION}
 EOF
 
-# Add the r2_profile credentials to AWS credentials
-cat > ~/.aws/credentials << EOF
+# Add profiles to AWS credentials
+cat >> ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+
 [r2_profile]
 aws_access_key_id = ${R2_ACCESS_KEY_ID}
 aws_secret_access_key = ${R2_SECRET_ACCESS_KEY}
+
+[b2_profile]
+aws_access_key_id = ${B2_ACCESS_KEY_ID}
+aws_secret_access_key = ${B2_SECRET_ACCESS_KEY}
 EOF
 
 # Set permissions
-chmod 600 ~/.aws/credentials
-chmod 600 ~/.aws/config
+chmod a+r ~/.aws/config
+chmod a+r ~/.aws/credentials
 
 # Setup GCS if credentials are provided
 if [ ! -z "${GCS_SERVICE_ACCOUNT_KEY}" ]; then
     echo "${GCS_SERVICE_ACCOUNT_KEY}" > /tmp/gcs-key.json
-    chmod 600 /tmp/gcs-key.json
+    chmod a+r /tmp/gcs-key.json
 fi
