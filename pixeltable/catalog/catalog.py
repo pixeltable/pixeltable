be75e21d894935c335c952cf55a0c1e065ebc4cd
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2025-10-27 17:07:49 -0700

    Fixes

--- pixeltable/catalog/catalog.py
+++ pixeltable/catalog/catalog.py
@@ -1898,6 +1898,7 @@ class Catalog:
                     .values({schema.Table.md: dataclasses.asdict(tbl_md)})
                     .where(schema.Table.id == tbl_id)
                 )
+                assert isinstance(result, sql.CursorResult)
                 assert result.rowcount == 1, result.rowcount
 
         # Construct and insert new table version record if requested.
@@ -1927,6 +1928,7 @@ class Catalog:
                     .values({schema.TableVersion.md: dataclasses.asdict(version_md)})
                     .where(schema.TableVersion.tbl_id == tbl_id, schema.TableVersion.version == version_md.version)
                 )
+                assert isinstance(result, sql.CursorResult)
                 assert result.rowcount == 1, result.rowcount
 
         # Construct and insert a new schema version record if requested.
