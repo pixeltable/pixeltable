113dfe133f6a5cf66940a437f87c6614fecfdffa
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2025-10-31 15:16:10 -0700

    test variants with default-only and non-default args

--- tests/functions/test_reve.py
+++ tests/functions/test_reve.py
@@ -14,15 +14,25 @@ _logger = logging.getLogger('pixeltable')
 @pytest.mark.expensive
 @rerun(reruns=3, reruns_delay=8)
 class TestReve:
-    def test_create_edit_remix(self, reset_db: None) -> None:
+    @pytest.mark.parametrize('default_params', [True, False], ids=['default_params', 'nondefault_params'])
+    def test_create_edit_remix(self, default_params: bool, reset_db: None) -> None:
         skip_test_if_no_client('reve')
 
         t = pxt.create_table('test_tbl', {'pixeltable_logo': pxt.Image})
-        t.add_computed_column(just_logo=(reve.edit(t.pixeltable_logo, 'extract the company logo and drop the name')))
+        t.add_computed_column(
+            just_logo=(
+                reve.edit(
+                    t.pixeltable_logo,
+                    'extract the company logo and drop the name',
+                    **({} if default_params else {'version': 'latest'}),
+                )
+            )
+        )
         t.add_computed_column(
             city_skyline=(
                 reve.create(
-                    'A futuristic city skyline with at night with a skyscraper featured prominently', aspect_ratio='1:1'
+                    'A futuristic city skyline with at night with a skyscraper featured prominently',
+                    **({} if default_params else {'aspect_ratio': '1:1', 'version': 'latest'}),
                 )
             )
         )
@@ -31,7 +41,7 @@ class TestReve:
                 reve.remix(
                     'Put a company logo from <img>0</img> on the skyscraper from <img>1</img>',
                     images=[t.just_logo, t.city_skyline],
-                    aspect_ratio='16:9',
+                    **({} if default_params else {'aspect_ratio': '16:9', 'version': 'latest'}),
                 )
             )
         )
