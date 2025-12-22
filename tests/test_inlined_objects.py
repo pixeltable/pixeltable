172e17986e690c95742b2eab8d89d20081eee60a
Marcel Kornacker <marcelk@gmail.com>
2025-12-22 13:27:08 -0800

    test failure

--- tests/test_inlined_objects.py
+++ tests/test_inlined_objects.py
@@ -2,16 +2,17 @@ import random
 import time
 from typing import Any
 
-import numpy as np
 import PIL.Image
+import numpy as np
+import pytest
 
 import pixeltable as pxt
 from pixeltable.env import Env
 from pixeltable.utils.local_store import LocalStore
-
 from .utils import ReloadTester, assert_columns_eq, inf_array_iterator, inf_image_iterator, validate_update_status
 
 
+@pytest.mark.expensive
 class TestInlinedObjects:
     def test_null_arrays(self, reset_db: None) -> None:
         t = pxt.create_table('test_tbl', {'i': pxt.Int, 'data': pxt.Array})
