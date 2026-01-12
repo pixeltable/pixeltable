84ddd0dfa48812cb910630682f54d13a6cf7f24b
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-01-12 15:44:24 -0800

    more debug logging

--- scripts/notebooks-markdown.sh
+++ scripts/notebooks-markdown.sh
@@ -12,12 +12,19 @@ if [ "$1" = "check" ]; then
     # (otherwise that's just some nice message that nbqa prints when the diff is empty)
     echo "find..."
     find . -type f -name "*.ipynb" | grep -v .ipynb_checkpoints
+    echo $?
     echo "nbqa..."
     nbqa mdformat --nbqa-md --nbqa-dont-skip-bad-cells --nbqa-diff ./docs/release/howto/cookbooks/data/data-sampling.ipynb
+    echo $?
     echo "awk..."
     echo 'hello' | awk 'END {exit 0}'
+    echo $?
+    echo "find + grep + nbqa..."
+    find . -type f -name "*.ipynb" | grep -v .ipynb_checkpoints | xargs nbqa mdformat --nbqa-md --nbqa-dont-skip-bad-cells --nbqa-diff 
+    echo $?
     echo "putting it all together..."
     find . -type f -name "*.ipynb" | grep -v .ipynb_checkpoints | xargs nbqa mdformat --nbqa-md --nbqa-dont-skip-bad-cells --nbqa-diff | awk 'END {exit (NR > 3 ? 1 : 0)}'
+    echo $?
 
 elif [ "$1" = "format" ]; then
     echo "Running format..."
