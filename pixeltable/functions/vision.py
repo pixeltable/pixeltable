0650b7ccad821dd0be703454818b2ce088b41cc0
Marcel Kornacker <marcelk@gmail.com>
2026-01-16 06:48:54 -0800

    PXT-961: correct support for alpha in draw_bounding_boxes()

--- pixeltable/functions/vision.py
+++ pixeltable/functions/vision.py
@@ -19,6 +19,8 @@ from typing import Any
 import numpy as np
 import PIL.Image
 import PIL.ImageColor
+import PIL.ImageDraw
+import PIL.ImageFont
 
 import pixeltable as pxt
 from pixeltable.utils.code import local_public_names
@@ -294,10 +296,13 @@ def __create_label_colors(labels: list[Any]) -> dict[Any, str]:
 def draw_bounding_boxes(
     img: PIL.Image.Image,
     boxes: list[list[int]],
+    *,
     labels: list[Any] | None = None,
     color: str | None = None,
     box_colors: list[str] | None = None,
+    alpha: float | None = None,
     fill: bool = False,
+    fill_alpha: float | None = None,
     width: int = 1,
     font: str | None = None,
     font_size: int | None = None,
@@ -309,7 +314,8 @@ def draw_bounding_boxes(
 
     Colors can be specified as common HTML color names (e.g., 'red') supported by PIL's
     [`ImageColor`](https://pillow.readthedocs.io/en/stable/reference/ImageColor.html#imagecolor-module) module or as
-    RGB hex codes (e.g., '#FF0000').
+    RGB/RGBA hex codes (e.g., '#FF0000', '#FF0000FF'). If opacity isn't specified in the color string and
+    `alpha`/`fill_alpha` is `None`, defaults to 1.0 for box borders and 0.5 for filled boxes.
 
     If no colors are specified, this function randomly assigns each label a specific color based on a hash of the label.
 
@@ -319,7 +325,11 @@ def draw_bounding_boxes(
         labels: List of labels for each bounding box.
         color: Single color to be used for all bounding boxes and labels.
         box_colors: List of colors, one per bounding box.
+        alpha: Opacity (0-1) of the bounding box borders and labels. If non-`None`, overrides any alpha in
+            `color`/`box_colors`.
         fill: Whether to fill the bounding boxes with color.
+        fill_alpha: Opacity (0-1) of the bounding box fill. If non-`None`, overrides any alpha in
+            `color`/`box_colors`.
         width: Width of the bounding box borders.
         font: Name of a system font or path to a TrueType font file, as required by
             [`PIL.ImageFont.truetype()`](https://pillow.readthedocs.io/en/stable/reference/ImageFont.html#PIL.ImageFont.truetype).
@@ -350,27 +360,42 @@ def draw_bounding_boxes(
         label_colors = __create_label_colors(labels)
         box_colors = [label_colors[label] for label in labels]
 
-    from PIL import ImageColor, ImageDraw, ImageFont
+    rgb_box_colors = [PIL.ImageColor.getrgb(c) for c in box_colors]
+    rgba_border_colors: list[tuple[int, int, int, int]]
+    if alpha is not None:
+        # override any alpha in rgb_box_colors
+        int_alpha = int(alpha * 255)
+        rgba_border_colors = [(c[0], c[1], c[2], int_alpha) for c in rgb_box_colors]
+    else:
+        # default to full opacity if alpha is missing
+        rgba_border_colors = [(*c, 255) if len(c) == 3 else c for c in rgb_box_colors]
+
+    rgba_fill_colors: list[tuple[int, int, int, int]] = []
+    if fill:
+        if fill_alpha is not None:
+            int_fill_alpha = int(fill_alpha * 255)
+            rgba_fill_colors = [(c[0], c[1], c[2], int_fill_alpha) for c in rgb_box_colors]
+        else:
+            rgba_fill_colors = [(c[0], c[1], c[2], 127) for c in rgb_box_colors]
 
     # set default font if not provided
-    txt_font: ImageFont.ImageFont | ImageFont.FreeTypeFont = (
-        ImageFont.load_default() if font is None else ImageFont.truetype(font=font, size=font_size or 10)
+    txt_font: PIL.ImageFont.ImageFont | PIL.ImageFont.FreeTypeFont = (
+        PIL.ImageFont.load_default() if font is None else PIL.ImageFont.truetype(font=font, size=font_size or 10)
     )
 
     img_to_draw = img.copy()
-    draw = ImageDraw.Draw(img_to_draw, 'RGBA' if fill else 'RGB')
+    draw = PIL.ImageDraw.Draw(img_to_draw, 'RGBA')
 
     # Draw bounding boxes
     for i, bbox in enumerate(boxes):
         # determine color for the current box and label
-        color = box_colors[i % len(box_colors)]
+        border_color = rgba_border_colors[i % len(box_colors)]
 
         if fill:
-            rgb_color = ImageColor.getrgb(color)
-            fill_color = (*rgb_color, 100)  # semi-transparent
-            draw.rectangle(bbox, outline=color, width=width, fill=fill_color)  # type: ignore[arg-type]
+            fill_color = rgba_fill_colors[i % len(box_colors)]
+            draw.rectangle(bbox, outline=border_color, width=width, fill=fill_color)  # type: ignore[arg-type]
         else:
-            draw.rectangle(bbox, outline=color, width=width)  # type: ignore[arg-type]
+            draw.rectangle(bbox, outline=border_color, width=width)  # type: ignore[arg-type]
 
     # Now draw labels separately, so they are not obscured by the boxes
     for bbox, label in zip(boxes, labels):
