e0cc060ef44eb32f02ba1bcd931f46439224ed39
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2026-01-23 15:27:12 -0800

    Add import sorting to notebooks

--- scripts/check-notebooks.sh
+++ scripts/check-notebooks.sh
@@ -17,7 +17,7 @@ fi
 
 echo "Checking code formatting ..."
 nbqa "ruff format --check --line-length=74" --nbqa-dont-skip-bad-cells docs/release || exit 1
-# nbqa "ruff check --select I --line-length=74" --nbqa-dont-skip-bad-cells docs/release || exit 1
+nbqa "ruff check --select I --line-length=74" --nbqa-dont-skip-bad-cells docs/release || exit 1
 
 # Run custom notebook checks
 echo "Running custom notebook checks ..."
