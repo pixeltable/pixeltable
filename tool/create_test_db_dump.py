8c9819bd32163387352949474065c0222040b688
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2026-01-31 15:55:54 -0800

    Cleanup

--- tool/create_test_db_dump.py
+++ tool/create_test_db_dump.py
@@ -258,8 +258,6 @@ class Dumper:
         assert t.base_table_image_rot.col.handle in project.stored_proxies
 
         # Various iterators
-        # TODO: audio_splitter and video_splitter won't work here because they produce local files as output,
-        #     so the dumps won't be portable - how to test this?
         pxt.create_view('string_splitter', t, iterator=pxtf.string.string_splitter(t.c1, 'sentence'))
         pxt.create_view('tile_iterator', t, iterator=pxtf.image.tile_iterator(t.c8, (64, 64), overlap=(16, 16)))
         pxt.create_view('frame_iterator_1', t, iterator=pxtf.video.frame_iterator(t.c10, fps=1))
@@ -274,6 +272,7 @@ class Dumper:
         # (the dumps won't be portable). But we can create filter views with no output, and that at least tests
         # that the iterator metadata survives the migration. By choosing an appropriate filter we ensure that
         # the insert statement in test_migration.py *does* produce data, providing further validation.
+        # TODO: "Bundling" local media files with the db dumps would increase test coverage.
         pxt.create_view(
             'audio_splitter',
             t.where(t.c2 >= len(SAMPLE_AUDIO_URLS)),
@@ -284,7 +283,9 @@ class Dumper:
         pxt.create_view(
             'video_splitter',
             t.where(t.c2 >= len(SAMPLE_VIDEO_URLS)),
-            iterator=pxtf.video.video_splitter(t.c10, duration=10.0, overlap=1.0, min_segment_duration=5.0, mode='fast'),
+            iterator=pxtf.video.video_splitter(
+                t.c10, duration=10.0, overlap=1.0, min_segment_duration=5.0, mode='fast'
+            ),
         )
         pxt.create_view(
             'video_splitter_2',
