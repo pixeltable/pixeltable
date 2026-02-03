07282845d085cce5420335ccbf6c6b1d7e6af40c
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2026-02-03 13:10:15 -0800

    Standardize names for runner configs

--- tests/test_video.py
+++ tests/test_video.py
@@ -785,7 +785,7 @@ class TestVideo:
             _ = pxt.create_view('s', t, iterator=video_splitter(t.video, segment_times=[1, 2], overlap=1))
 
     @pytest.mark.skipif(
-        os.environ.get('PXTTEST_CI_OS') == 'ubuntu-x64-t4', reason='Fonts not available on t4 CI instances'
+        't4' in os.environ.get('PXTTEST_CI_OS'), reason='Fonts not available on t4 CI instances'
     )
     def test_overlay_text(self, uses_db: None, tmp_path: Path) -> None:
         t = pxt.create_table('videos', {'video': pxt.Video})
