7aab9d9e89c676de7013f5f27d7c283537598beb
Marcel Kornacker <marcelk@gmail.com>
2026-02-24 19:10:20 -0800

    linter error

--- pixeltable/runtime.py
+++ pixeltable/runtime.py
@@ -83,7 +83,7 @@ class Runtime:
             self._event_loop.slow_callback_duration = 3600
 
         # TODO: remove unconditional nest_asyncio.apply()
-        if True or Env.get().is_notebook():
+        if True or Env.get().is_notebook():  # noqa: SIM222
             # Jupyter notebooks have their own event loop, which we need to patch to allow nested run_until_complete()
             nest_asyncio.apply(self._event_loop)
         if _logger.isEnabledFor(logging.DEBUG):
