e7eafd712ded422d8c7331c7be9a8d6a7cafc426
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-04-21 16:08:12 -0700

    fix incorrect merge

--- pixeltable/catalog/catalog.py
+++ pixeltable/catalog/catalog.py
@@ -1968,8 +1968,10 @@ class Catalog:
 
             if tv.anchor_tbl_id is None:
                 # live non-replica table; compare our cached TableMd.current_version/view_sn to what's stored
-                current_version, view_sn = row.md['current_version'], row.md['view_sn']
-                if current_version != tv.version or view_sn != tv.tbl_md.view_sn:
+                is_versioned = row.md.get('is_versioned', True)
+                current_version = row.md['current_version']
+                view_sn = row.md['view_sn']
+                if (is_versioned and current_version != tv.version) or view_sn != tv.tbl_md.view_sn:
                     _logger.debug(
                         f'reloading metadata for live table {key.tbl_id} '
                         f'(cached/current version: {tv.version}/{current_version}, '
