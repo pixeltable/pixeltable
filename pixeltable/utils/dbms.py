0b70dcc2fa3648fb1a68bf0b098dc2759a7516c9
Christopher Pestano <cpestano@pixeltable.com>
2026-04-22 15:55:06 -0700

    Support utf-8

--- pixeltable/utils/dbms.py
+++ pixeltable/utils/dbms.py
@@ -1,4 +1,5 @@
 import abc
+import platform
 
 import sqlalchemy as sql
 
@@ -46,7 +47,12 @@ class PostgresqlDbms(Dbms):
         return f'DROP DATABASE {database}'
 
     def create_db_stmt(self, database: str) -> str:
-        return f"CREATE DATABASE {database} ENCODING 'utf-8' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0"
+        # Windows' libc rejects "C.UTF-8"; UCRT accepts ".UTF-8" for LC_CTYPE.
+        lc_ctype = '.UTF-8' if platform.system() == 'Windows' else 'C.UTF-8'
+        return (
+            f"CREATE DATABASE {database} ENCODING 'utf-8' "
+            f"LC_COLLATE 'C' LC_CTYPE '{lc_ctype}' TEMPLATE template0"
+        )
 
     def default_system_db_url(self) -> str:
         a = self.db_url.set(database='postgres').render_as_string(hide_password=False)
