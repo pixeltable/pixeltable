6fafbb7a367c3e08eeac943579cdf7902f332388
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-04-30 10:01:46 -0700

    unversioned tbl test -- stop checking internals

--- tests/test_unversioned_table.py
+++ tests/test_unversioned_table.py
@@ -1,9 +1,7 @@
 import pytest
-import sqlalchemy as sql
 
 import pixeltable as pxt
 import pixeltable.exceptions as excs
-from pixeltable.runtime import get_runtime
 
 from .utils import ReloadTester, validate_update_status
 
@@ -35,33 +33,7 @@ class TestUnversionedTable:
         assert rows[1]['c0'] == 1 and rows[1]['c1'] == 'b'
         reload_tester.run_reload_test()
 
-        # Verify the store table state. Check for unexpected rows and columns.
-        store_name = tbl._tbl_version.get().store_tbl.sa_tbl.name
-
-        with get_runtime().begin_xact() as conn:
-            inspector = sql.inspect(conn)
-            col_names = {col['name'] for col in inspector.get_columns(store_name)}
-            col_names.remove('rowid')
-            assert all(name.startswith('col_') for name in col_names), col_names
-
-            pk_constraint = inspector.get_pk_constraint(store_name)
-            assert pk_constraint['constrained_columns'] == ['rowid'], pk_constraint
-
-            row_count = conn.execute(sql.text(f'SELECT COUNT(*) FROM "{store_name}"')).scalar()
-            assert row_count == 2
-
-            # Verify that we did not generate unexpected version entries for this table
-            for system_tbl in ('tableversions', 'tableschemaversions'):
-                row_count = conn.execute(
-                    sql.text(f"SELECT COUNT(*) FROM {system_tbl} where tbl_id = '{tbl._id}'")
-                ).scalar()
-                assert row_count == 1, (system_tbl, row_count)
-
         pxt.drop_table(tbl)
-
-        with get_runtime().begin_xact() as conn:
-            inspector = sql.inspect(conn)
-            assert not inspector.has_table(store_name)
 
     def test_select_where(self, uses_db: None) -> None:
         schema = {'c_int': pxt.Int, 'c_str': pxt.String, 'c_float': pxt.Float, 'c_bool': pxt.Bool}
