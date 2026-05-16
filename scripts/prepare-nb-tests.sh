aefb7b0fb9d10519539bbedd20cb576d222b2285
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-05-15 18:00:24 -0700

    order

--- scripts/prepare-nb-tests.sh
+++ scripts/prepare-nb-tests.sh
@@ -6,11 +6,11 @@ SKIP_NOTEBOOKS=(
     working-with-bfl                # [PXT-1111] Out of credits
     working-with-fabric             # [PXT-1113] Requires Microsoft Fabric environment
     working-with-fiftyone           # [PXT-1117] Voxel51 is currently omitted from our dev env for security reasons
+    working-with-fireworks          # [PXT-1160] Model not found, inaccessible, and/or not deployed
     working-with-tigris             # [PXT-1122] Hard-codes getpass() calls for credentials and bucket
     working-with-reve               # [PXT-1116] Out of credits
     working-with-runwayml           # [PXT-1120] RunwayML integration is very broken
     working-with-twelvelabs         # [PXT-1119] Exceeds rate limit
-    working-with-fireworks          # [PXT-1160] Model not found, inaccessible, and/or not deployed
 )
 
 # Notebooks that are skipped unless --include-expensive is passed
