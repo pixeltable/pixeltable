f3468aea8fdc80e4d8484739560c28ae5a809e6e
Marcel Kornacker <marcelk@gmail.com>
2026-05-17 07:59:05 -0700

    docs

--- pcli/server/app.py
+++ pcli/server/app.py
@@ -4,29 +4,12 @@ from fastapi.responses import JSONResponse
 
 from pixeltable import exceptions as excs
 
-from .routes import (
-    columns,
-    count,
-    describe,
-    drop,
-    env,
-    errors,
-    get,
-    health,
-    history,
-    idxs,
-    ls,
-    move,
-    revert,
-    rows,
-    status,
-)
+from .routes import router
 
 
 def create_app() -> FastAPI:
     app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
-    for r in (health, ls, describe, errors, history, columns, idxs, rows, status, env, count, get, drop, move, revert):
-        app.include_router(r.router)
+    app.include_router(router)
 
     @app.exception_handler(excs.Error)
     def pxt_error_handler(_request: Request, exc: excs.Error) -> JSONResponse:
