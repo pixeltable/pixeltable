5a2b5aae06afe9e93caf94fc375707ffe8ea4e37
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-05-26 17:21:56 -0700

    [PXT-1172]

--- pixeltable/utils/fault_injection.py
+++ pixeltable/utils/fault_injection.py
@@ -13,6 +13,7 @@ class FaultLocation(Enum):
     CATALOG_FINALIZE_PENDING_OPS_NON_XACT = auto()
     CATALOG_LOAD_VIEW_OP_EXEC = auto()
     SCHEDULER_RATE_LIMITS_AEXEC = auto()
+    STORE_CREATE_SA_TBL_AFTER_EXISTS_CHECK = auto()
     TEST = auto()
 
 
