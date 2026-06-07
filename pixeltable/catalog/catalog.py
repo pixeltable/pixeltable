0fffde6f05d25a5026060c30601edcb81e80a5ec
sergey-mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-06-07 15:53:24 -0700

    Verify all necessary locks on retry_loop re-entry (#1391)
    - If already in a retry loop, validate that the outer one acquired all
    write locks. Same check that already exists in `begin_xact()`
    - Make validation stronger by taking into account `lock_mutable_tree`
    - On the happy path, this check should add no new I/O

--- pixeltable/catalog/catalog.py
+++ pixeltable/catalog/catalog.py
@@ -95,6 +95,7 @@ def retry_loop(
             cat = get_runtime().catalog
             # retry_loop() is reentrant
             if cat._in_retry_loop:
+                cat._check_write_locks(write_tvps or [], write_tbl_ids or [], lock_mutable_tree)
                 return op(*args, **kwargs)
 
             num_retries = 0
@@ -366,11 +367,7 @@ class Catalog:
         read_tbl_ids = read_tbl_ids or []
         write_tbl_ids = write_tbl_ids or []
         if get_runtime().in_xact:
-            # make sure all required locks are already being held
-            for tvp in write_tvps:
-                assert tvp.tbl_id in self._x_locked_tbl_ids, f'{tvp.tbl_id} not locked: {self._x_locked_tbl_ids}'
-            for tbl_id in write_tbl_ids:
-                assert tbl_id in self._x_locked_tbl_ids, f'{tbl_id} not locked: {self._x_locked_tbl_ids}'
+            self._check_write_locks(write_tvps, write_tbl_ids, lock_mutable_tree)
             yield get_runtime().conn
             return
 
@@ -716,6 +713,26 @@ class Catalog:
                     )
                 )
         return locked
+
+    def _check_write_locks(
+        self, write_tvps: Collection[TableVersionPath], write_tbl_ids: Collection[UUID], lock_mutable_tree: bool
+    ) -> None:
+        """Asserts that all specified write targets (and their mutable trees, if lock_mutable_tree) are locked."""
+
+        def assert_tree_locked(tbl_id: UUID) -> None:
+            tree = self._get_mutable_tree(tbl_id)
+            assert tree.issubset(self._x_locked_tbl_ids), (
+                f'mutable tree of {tbl_id} not fully locked: {tree - self._x_locked_tbl_ids}'
+            )
+
+        for tvp in write_tvps:
+            assert tvp.tbl_id in self._x_locked_tbl_ids, f'{tvp.tbl_id} not locked: {self._x_locked_tbl_ids}'
+            if lock_mutable_tree:
+                assert_tree_locked(tvp.tbl_id)
+        for tbl_id in write_tbl_ids:
+            assert tbl_id in self._x_locked_tbl_ids, f'{tbl_id} not locked: {self._x_locked_tbl_ids}'
+            if lock_mutable_tree:
+                assert_tree_locked(tbl_id)
 
     def _refresh_tbl_cache(self, *, tbl_id: UUID, check_pending_ops: bool = True) -> None:
         """
