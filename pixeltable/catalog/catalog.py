b075db5c3e74e4a621cb9e5fd1371a0aa757d664
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-06-12 22:06:54 -0700

    .

--- pixeltable/catalog/catalog.py
+++ pixeltable/catalog/catalog.py
@@ -941,9 +941,9 @@ class Catalog:
 
             except Exception as e:
                 if excs.is_table_not_found_error(e):
-                    _logger.error(f'Finalize pending ops({tbl_id}): table not found', exc_info=True)
+                    _logger.error(f'Finalize pending ops({tbl_id}): table not found, exiting', exc_info=True)
                     # nothing to do
-                    return
+                    return None
 
                 if not is_rollback and tbl_md is not None and tbl_md.pending_stmt.can_abort():
                     _logger.error(
