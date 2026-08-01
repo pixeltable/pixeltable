75106b7555a2ec1776e407b7868201fdb939bb02
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-07-31 17:36:41 -0700

    var rename

--- pixeltable/catalog/table_version.py
+++ pixeltable/catalog/table_version.py
@@ -1807,8 +1807,8 @@ class TableVersion:
 
         Tables created before this property was recorded in the metadata return False.
         """
-        md_value = self._tbl_md.default_idxs_enabled
-        return False if md_value is None else md_value
+        enabled = self._tbl_md.default_idxs_enabled
+        return False if enabled is None else enabled
 
     def bump_version(self, timestamp: float | None = None, *, bump_schema_version: bool) -> None:
         """
