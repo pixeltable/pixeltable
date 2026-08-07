b0d3af3191c9b135131c6e81be23d4206cab0a8f
Aaron Siegel <2014957+aaron-siegel@users.noreply.github.com>
2026-08-07 12:26:00 -0700

    Review feedback

--- tests/tool/test_db_dump_tool.py
+++ tests/tool/test_db_dump_tool.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+import sys
 import sysconfig
 
 import pytest
@@ -9,9 +10,9 @@ from ..utils import skip_test_if_not_installed
 
 @pytest.mark.skipif(platform.system() == 'Windows', reason='Tool is not supported on Windows')
 @pytest.mark.skipif(sysconfig.get_platform() == 'linux-aarch64', reason='Tool is not supported on Linux ARM')
-@pytest.mark.very_expensive
+@pytest.mark.expensive
 class TestDbDumpTool:
     def test_db_dump_tool(self) -> None:
         skip_test_if_not_installed('transformers')
         # A generous timeout to allow for a large HF download
-        subprocess.run(('python', 'tool/create_test_db_dump.py'), check=True, timeout=900)
+        subprocess.run((sys.executable, 'tool/create_test_db_dump.py'), check=True, timeout=900)
