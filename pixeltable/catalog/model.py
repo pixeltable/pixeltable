2f9af5f53a55de377b065a6c53915e2e535896cc
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-08-19 18:25:02 -0700

    two separate errors for operational views

--- pixeltable/catalog/model.py
+++ pixeltable/catalog/model.py
@@ -552,11 +552,13 @@ class TableModelMeta(type):
                     )
 
             # A view is always of the same kind as its base. Operational views aren't supported yet.
-            if base is not None and (
-                not _is_data_versioned or not base.from_clause.__table_spec__['is_data_versioned']
-            ):
+            if base is not None and not _is_data_versioned:
                 raise excs.RequestError(
                     excs.ErrorCode.UNSUPPORTED_OPERATION, f'{display_name}: operational views are not supported yet.'
+                )
+            if base is not None and not base.from_clause.__table_spec__['is_data_versioned']:
+                raise excs.RequestError(
+                    excs.ErrorCode.UNSUPPORTED_OPERATION, f'{display_name}: the base table and the view have mismatching is_data_versioned'
                 )
 
             # Validate iterator
