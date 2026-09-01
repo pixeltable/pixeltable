ceaa65c3f0d04c59ab36a4cf1d0aadbbbc33dff7
Sergey Mkhitaryan <mkhitaryan.sergey@gmail.com>
2026-08-31 22:28:06 -0700

    reverted to typing.Mapping

--- pixeltable/catalog/globals.py
+++ pixeltable/catalog/globals.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import dataclasses
 import enum
 import itertools
-from collections.abc import Mapping
-from typing import TYPE_CHECKING, Any, NamedTuple, TypeVar, cast
+from typing import TYPE_CHECKING, Any, Mapping, NamedTuple, TypeVar, cast
 
 from typing_extensions import TypeForm
 
